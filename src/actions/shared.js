e588f61e3b6e2aaf83788d2c5980609ddd5cdb62
adeiltonsousa <adeiltonsousaxavier@gmail.com>
2019-03-01 17:55:12 -0300

    Criação do Estado inicial, com users, tweets e AUTHED_ID e imports

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from '../actions/users'
+import { receiveTweets } from '../actions/tweets'
+import { steAuthedUser } from '../actions/authedUsers'
+
+const AUTHED_ID = 'tylermcginnis'
+
+export function handleInitialData () {
+    return (dispatch) => {
+        return getInitialData()
+        .then(({ users, tweets }) => {
+            dispatch(receiveUsers(users))
+            dispatch(receiveTweets(tweets))
+            dispatch(steAuthedUser(AUTHED_ID))
+        })
+    }
+}
