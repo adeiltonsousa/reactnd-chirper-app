ff2ee75ae87b839db22ed7814386cf1b5d6bd3b9
Adeilton Sousa <adeiltonsousaxavier@gmail.com>
2019-03-03 20:37:11 -0300

    Validação de Tweet inexistente (Tweets.js) + verificação do parentTweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,7 +5,12 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
     render () {
-        console.log(this.props)
+        const { tweet } = this.props
+
+        if (tweet === null) {
+            return <p>This Tweet doesn't existd</p>
+        }
+
         return (
             <div className='tweet'>
 
@@ -14,12 +19,20 @@ class Tweet extends Component {
     }
 }
 
+/* 
+mapStateToProps aceita dois argumentos:
+- o estado do armazenador
+- as props que foram passadas para o componente Tweet
+*/
 function mapStateToProps ({authedUser, users, tweets}, { id }) {
     const tweet = tweets[id]
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
     return {
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet
+            ?   formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+            :   null
     }
 }
 
